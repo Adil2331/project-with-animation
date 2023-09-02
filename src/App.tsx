505fc13344f5e283c98b46480d62af54064b8324
Adil Temirgaliyev <126072562+Adil2331@users.noreply.github.com>
2023-09-02 22:07:12 +0300

    added smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
-import './App.css'
+import {
+  motion,
+  useSpring,
+  useTransform,
+  useViewportScroll,
+} from 'framer-motion'
 import BestTeam from './components/BestTeam/indext'
 import Menu from './components/Menu'
+import OurProfession from './components/OurProfession'
+import { useLayoutEffect, useRef, useState } from 'react'
 
 function App() {
+  const { scrollY } = useViewportScroll()
+
+  const containerRef = useRef<HTMLDivElement>(null)
+  const [containerSize, setContainerSize] = useState<number>(0)
+
+  useLayoutEffect(() => {
+    const getSize = () => {
+      return containerRef.current?.getBoundingClientRect().height ?? 0
+    }
+
+    setContainerSize(getSize())
+
+    window.addEventListener('resize', () => {
+      setContainerSize(getSize())
+    })
+  }, [])
+
+  const y = useTransform(scrollY, [0, containerSize], [0, -containerSize])
+
+  const style = useSpring(y, { stiffness: 100, damping: 30 })
+
   return (
     <div className="App">
-      <Menu />
-      <BestTeam />
+      <motion.div ref={containerRef} style={{ y: style }}>
+        <Menu />
+        <BestTeam />
+        <OurProfession />
+      </motion.div>
     </div>
   )
 }
-
 export default App

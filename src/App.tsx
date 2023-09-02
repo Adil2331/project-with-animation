7117dc266b805f7e83be8da1e07e0bbe10cb1165
Adil Temirgaliyev <126072562+Adil2331@users.noreply.github.com>
2023-09-02 20:16:45 +0300

    added BestTeam component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import BestTeam from './components/BestTeam/indext'
 import Menu from './components/Menu'
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      <BestTeam />
     </div>
   )
 }
---
+++ src/components/BestTeam/indext.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './style.css'
+
+const BestTeam = () => {
+  return (
+    <div className="BestTeam container">
+      <div className="team_inner">
+        <h1 className="team_title">
+          Build the <span>best teams</span> with great people
+        </h1>
+        <p className="team_text">
+          Recruitment of managers, experts and senior executives in Data,
+          Innovation, Digital, Marketing and Communication.
+        </p>
+      </div>
+      <div className="team_img">
+        <img
+          src="https://cherchesusan.com/wp-content/uploads/fly-images/419/intro-1-1-1-0x800.png"
+          alt=""
+          className="team_img-item"
+        />
+        {/* <div>
+          <img
+            src="https://cherchesusan.com/wp-content/uploads/fly-images/420/intro-1-2-1-120x220-c.png"
+            alt=""
+            className="team_img-item"
+          />
+          <img
+            src="https://cherchesusan.com/wp-content/uploads/fly-images/421/intro-1-3-1-0x400.png"
+            alt=""
+            className="team_img-item"
+          />
+        </div> */}
+      </div>
+    </div>
+  )
+}
+
+export default BestTeam
+
+// https://cherchesusan.com/wp-content/uploads/fly-images/486/h11-1-0x1200.png

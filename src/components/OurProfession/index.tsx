4fe35eaf2d7d0bad3fda0dcd15f1209cb6b5d457
Adil Temirgaliyev <126072562+Adil2331@users.noreply.github.com>
2023-09-05 20:51:43 +0300

    updated components

--- src/components/OurProfession/index.tsx
+++ src/components/OurProfession/index.tsx
@@ -25,7 +25,7 @@ const OurProfession = () => {
         initial="hidden"
         animate={inView ? 'visible' : 'hidden'}
         variants={{
-          hidden: { opacity: 0, x: -50 },
+          hidden: { opacity: 0, x: -500 },
           visible: { opacity: 1, x: 0 },
         }}
         transition={{ duration: 0.7 }}
